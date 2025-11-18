ce25fee4620f80582c2fcf30134a7ae49d475800
Artur Podsiadły <artpods56@gmail.com>
2025-11-18 12:23:33 +0100

    chore: remove all tests for template-task-branch

--- src/ocr_kul/entrypoints/celery_app.py
+++
@@ -1,3 +0,0 @@
-from celery import Celery
-
-app = Celery("ocr_kul", broker="amqp://guest@localhost")
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add the project root to sys.path so that 'tests' module can be imported
-project_root = Path(__file__).parent.parent
-if str(project_root) not in sys.path:
-    sys.path.insert(0, str(project_root))
