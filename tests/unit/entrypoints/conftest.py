bcc2b190462d995a78bde67ac71fce1b20186851
Artur Podsiadły <artpods56@gmail.com>
2025-12-01 11:19:44 +0100

    test: add fixture to specify the backend used for async tests

--- tests/unit/entrypoints/conftest.py
+++ tests/unit/entrypoints/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 from typing import Literal
 
 import pytest_asyncio
@@ -6,8 +8,14 @@ from httpx import ASGITransport, AsyncClient
 from kul_ocr.entrypoints.api import app
 
 
+@pytest.fixture
+def anyio_backend() -> str:
+    """Specifies the backend to use for asynchronous tests."""
+    return "asyncio"
+
+
 @pytest_asyncio.fixture(autouse=True)
-async def client(anyio_backend: Literal["trio", "asyncio"]):
+async def client(anyio_backend: Literal["asyncio"]):
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
