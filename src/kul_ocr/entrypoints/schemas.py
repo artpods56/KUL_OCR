8d70a852ad75c53a7f4f0eccdccd2f5b17b79e39
ggWOLF <zdybjan8@gmail.com>
2025-12-05 16:07:19 +0100

    chore: add types to generic class type

--- src/kul_ocr/entrypoints/schemas.py
+++ src/kul_ocr/entrypoints/schemas.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from tarfile import data_filter
 
 from pydantic import BaseModel
 
-from kul_ocr.domain import model    
-        
+from kul_ocr.domain import model
+
+
 class DocumentResponse(BaseModel):
     id: str
     file_path: str
@@ -16,7 +16,7 @@ class DocumentResponse(BaseModel):
     def from_domain(
         cls,
         document: model.Document,
-        ) -> "DocumentResponse":
+    ) -> "DocumentResponse":
         return cls(
             id=document.id,
             file_path=document.file_path,
@@ -27,24 +27,28 @@ class DocumentResponse(BaseModel):
 
     class Config:
         use_enum_values = False
-        
+
+
 class OcrResultResponse(BaseModel):
     id: str
-    job_id:str
+    job_id: str
     content: str
     creation_time: datetime
 
     @classmethod
     def from_domain(
         cls,
-        result: model.OCRResult,
-        ) -> "OcrResultResponse":
+        result: model.OCRResult[model.SimpleOCRValue],
+    ) -> "OcrResultResponse":
         return cls(
             id=result.id,
             job_id=result.job_id,
-            content=result.content.content if hasattr(result.content,"content") else str(result.content),
+            content=result.content.content
+            if hasattr(result.content, "content")
+            else str(result.content),
             creation_time=result.creation_time,
         )
+
 
 class DocumentWithResultResponses(BaseModel):
     id: str
@@ -52,13 +56,13 @@ class DocumentWithResultResponses(BaseModel):
     file_type: model.FileType
     uploaded_at: datetime
     file_size_bytes: int
-    latest_result: OcrResultResponse | None=None
-    
+    latest_result: OcrResultResponse | None = None
+
     @classmethod
     def from_domain(
         cls,
         document: model.Document,
-        result: model.OCRResult | None=None,
+        result: model.OCRResult[model.SimpleOCRValue],
     ) -> "DocumentWithResultResponses":
         return cls(
             id=document.id,
@@ -66,8 +70,8 @@ class DocumentWithResultResponses(BaseModel):
             file_type=document.file_type,
             uploaded_at=document.uploaded_at,
             file_size_bytes=document.file_size_bytes,
-            latest_result=(
-                OcrResultResponse.from_domain(result) if result else None),
+            latest_result=(OcrResultResponse.from_domain(result) if result else None),
         )
+
     class Config:
-        use_enum_values=False
+        use_enum_values = False
